df0634d89201bb755c3a926b0210c98ebf9c1806
Jason Robinson <jasonsrobinson@protonmail.com>
2023-09-07 23:30:21 -0500

    Updated Javadoc for generating

--- ch03/WordTool/src/WordTool.java
+++ ch03/WordTool/src/WordTool.java
@@ -9,14 +9,31 @@ certain word or letter in the text, how many words we have used so far, and more
  */
 
 /**
- *
- * @author PacktPub
+ * A class that can perform various statistical analysis on text.
+ * 
+ * Usage:
+ * <code>
+ *   WordTool wt = new WordTool();
+ *   wt.wordCount("An example corpus");
+ * </code>
+ * 
+ * @author Jason Robinson
+ * @version 0.1, 09/07/23
  */
 public class WordTool {
 
+    /**
+     * Class constructor.
+     */
     public WordTool() {
     }
 
+    /**
+     * Demonstrates how to use <code>WordTool</code> and it's expected results.
+     *
+     * @param args optional command line arguments passed to the application.
+     *             Has no use or bearing on the demonstration.
+     */
     public static void main(String[] args) {
         WordTool wt = new WordTool();
         String text = "The river carried the memories from her childhood.";
@@ -28,10 +45,16 @@ public class WordTool {
     }
 
     /**
-     * Count the number of words within a string.
+     * Counts the number of words within a string.
+     * 
+     * Words are considered groups of characters / symbols succeeded, preceded
+     * or surrounded by white spaces.
+     * 
+     * given: <code>"An example corpus"</code>, expect: <code>3</code>
+     * given: <code>""</code>, expect: <code>0</code>
      *
-     * @param s The string to be used
-     * @return The number of words found in the string
+     * @param s the string to be used
+     * @return the number of words found in the given string
      */
     public int wordCount(String s) {
         int count = 0; // variable to count words
@@ -49,10 +72,15 @@ public class WordTool {
     }
 
     /**
-     * Count the number of letters in a string, and add the ability to count
-     * both with and without white space characters
+     * Counts the number of letters in a string.
+     * 
+     * The ability to count both with and without white space characters is
+     * given by the flag <code>withSpaces</code>
      *
-     * @param s The data to be read
+     * given: <code>"An example corpus", false</code>, expect: <code>15</code>
+     * given: <code>"An example corpus", true</code>, expect: <code>17</code>
+     * 
+     * @param s the data to be read
      * @param withSpaces flag to determine if white spaces are to be treated as
      * symbols
      * @return The amount of symbols found in the given string
@@ -77,12 +105,39 @@ public class WordTool {
     }
     
     /**
-     * Calculates the frequency (how often) of a certain symbol.
+     * Calculates the frequency of a certain symbol appearing within a given text.
      * 
-     * @param s The text data containing the symbol
-     * @param symbol The symbol to search for
-     * @param whiteSpaces Set to true if the symbol being counted is a white space
-     * @return The frequency of the specified symbol contained in the given data
+     * Given an example to search for the letter <code>'e'</code>, not treating
+     * white spaces as symbols:
+     * <code>
+     *   WordTool wt = new WordTool();
+     *   String text = "The river carried the memories from her childhood.";
+     *   int frequency = wt.symbolFrequency(text, 'e', false);
+     * </code>
+     * Expect: <code>frequency</code> => <code>7</code>
+     * 
+     * Given n example to search for the letter <code>'h'</code>, this time, treating
+     * white spaces as symbols, the effect is the same as the previous example:
+     * <code>
+     *   WordTool wt = new WordTool();
+     *   String text = "The river carried the memories from her childhood.";
+     *   int frequency = wt.symbolFrequency(text, 'h', true);
+     * </code>
+     * Expect: <code>frequency</code> => <code>5</code>
+     * 
+     * Given n example to search for white spaces <code>' '</code>, this time, treating
+     * white spaces as symbols:
+     * <code>
+     *   WordTool wt = new WordTool();
+     *   String text = "The river carried the memories from her childhood.";
+     *   int frequency = wt.symbolFrequency(text, ' ', true);
+     * </code>
+     * Expect: <code>frequency</code> => <code>7</code>
+     * 
+     * @param s the text data containing the symbol
+     * @param symbol the symbol to search for
+     * @param whiteSpaces a flag to determine whether white spaces should be counted
+     * @return how often the specified symbol appears in the given data
      */
     public int symbolFrequency(String s, char symbol, boolean whiteSpaces) {
         int count = 0;
