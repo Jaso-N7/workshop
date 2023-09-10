59608c3edf482ba14ffcf2f87802410635e38c23
Jason Robinson <jasonsrobinson@protonmail.com>
2023-09-10 12:13:47 -0500

    Examples on how to compare Arrays

--- ch04/CollectionsAndAPIs/src/main/java/com/mindfulengineering/learn/arrays/Example03.java
+++ ch04/CollectionsAndAPIs/src/main/java/com/mindfulengineering/learn/arrays/Example03.java
@@ -24,5 +24,32 @@ public class Example03 {
      */
     public static void main(String[] args) {
         
+        int[] numbers1 = new int[3];
+        Arrays.fill(numbers1, 1);
+        
+        // comparing numbers1 to numbers2 using Arrays.equals
+        int[] numbers2 = {0, 0, 0};
+        boolean comparison = Arrays.equals(numbers1, numbers2);
+        System.out.format("Arrays.equals(%s, %s) : %s%n",
+                Arrays.toString(numbers1),
+                Arrays.toString(numbers2), 
+                comparison);
+        
+        // comparing numbers1 to numbers3
+        int[] numbers3 = {1, 1, 1};
+        comparison = Arrays.equals(numbers1, numbers3);
+        System.out.format("Arrays.equals(%s, %s) : %s%n",
+                Arrays.toString(numbers1),
+                Arrays.toString(numbers3), 
+                comparison);
+        
+        // comparing numbers1 to numbers4
+        int[] numbers4 = {1, 1};
+        comparison = Arrays.equals(numbers1, numbers4);
+        System.out.format("Arrays.equals(%s, %s) : %s%n",
+                Arrays.toString(numbers1),
+                Arrays.toString(numbers4), 
+                comparison);
+        
     }
 }
