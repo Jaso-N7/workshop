3c1661ba619224e298b93ed9f35a5100d022f7b3
Jason Robinson <jasonsrobinson@protonmail.com>
2023-09-15 12:09:36 -0500

    Learning how to log Exceptions

---
+++ ch05/LoggingExceptions/src/main/java/com/mindfulengineering/loggingexceptions/LoggingExceptions.java
@@ -0,0 +1,25 @@
+package com.mindfulengineering.loggingexceptions;
+
+import java.util.logging.*;
+
+/**
+ * Class that assists with Logging Exceptions
+ * 
+ * @author Jason Robinson
+ * @version 0.1, 09/15/23
+ */
+public class LoggingExceptions {
+
+    public static void main(String[] args) {
+        
+        Logger logger = Logger.getAnonymousLogger();
+        
+        // Provoking an exception
+        String s = null;
+        try {
+            System.out.println(s.length());
+        } catch (NullPointerException np) {
+            logger.log(Level.SEVERE, "Exception happened", np);
+        }
+    }
+}
