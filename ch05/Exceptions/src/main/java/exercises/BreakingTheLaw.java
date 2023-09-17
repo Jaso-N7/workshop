6b46cade963adf90d14a181a464d22bf9daaa7a0
Jason Robinson <jasonsrobinson@protonmail.com>
2023-09-17 11:45:29 -0500

    Updated javadoc

--- ch05/Exceptions/src/main/java/exercises/BreakingTheLaw.java
+++ ch05/Exceptions/src/main/java/exercises/BreakingTheLaw.java
@@ -1,7 +1,8 @@
 package exercises;
 
 /**
- *
+ * Example of how to Break and Fix your code using your own custom Exception.
+ * 
  * @author Jason Robinson
  * @version 0.1, 09/15/23
  */
@@ -15,16 +16,20 @@ public class BreakingTheLaw {
         
         /**
          * Class constructor
+         * @param message the exception message
          */
         public MyException(String message) { super(message); }
         
         /**
          * Class constructor
+         * @param message the exception message
+         * @param cause the stack trace for the exception
          */
         public MyException(String message, Throwable cause) { super(message, cause); }
         
         /**
          * Class constructor
+         * @param cause the stack trace for the exception
          */
         public MyException(Throwable cause) { super(cause); }
     }
